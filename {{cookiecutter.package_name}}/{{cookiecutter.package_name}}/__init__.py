6fe97cfb8193d49bbae0b93decec1367540e4b9a
Nabeel Shahzad <nshahzad@gmail.com>
2017-02-01 22:44:42 -0600

    tempalte fix

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,4 +1,4 @@
-"""{{ cookiecutter.package_name }} - {{ cookiecutter.package_description }}"""
+# {{ cookiecutter.package_name }}
 
 __author__ = '{{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>'
 __version__ = '{{ cookiecutter.package_version }}'
